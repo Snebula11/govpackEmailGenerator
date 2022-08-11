ecbccf8670221e2036f077aeb91939fb0dde75c8
Snebula11 <benjamin@swedberg.us>
2022-08-11 13:12:10 -0500

    new emails!

--- main.py
+++ main.py
@@ -5,15 +5,18 @@ import fill_template as ft
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # user input: take state to generate all emails for
-    inp = input("Type the postal abbr. for the state you need emails: ").upper()
+    # inp = input("Type the postal abbr. for the state you need emails: ").upper()
 
-    output_filepath = 'emails/' + inp.lower() + '_emails.csv'
+    for inp in helpers.states.keys():
+        output_filepath = 'emails/' + inp.lower() + '_emails.csv'
 
-    url = 'https://raw.githubusercontent.com/Snebula11/oklahama-cleaner/main/data/' \
-          + inp.upper() \
-          + '/' \
-          + inp.lower() \
-          + '_output.csv'
-    df = helpers.get_data(url)
+        url = 'https://raw.githubusercontent.com/Snebula11/oklahama-cleaner/main/data/' \
+              + inp.upper() \
+              + '/' \
+              + inp.lower() \
+              + '_output.csv'
+        df = helpers.get_data(url)
 
-    ft.fill_template(df).to_csv(output_filepath, index=False, encoding='utf-8')
+        print(f'making {inp} emails')
+
+        ft.fill_template(df).to_csv(output_filepath, index=False, encoding='utf-8')
